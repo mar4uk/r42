330d20752039e18184c21d6a329359d1ea2a6c2b
Irina Marchuk <mar4ukira@gmail.com>
2018-05-19 22:12:28 +0300

    fix first request

--- app/controllers/selections-chart.js
+++ app/controllers/selections-chart.js
@@ -1,7 +1,9 @@
 const selectionsProvider = require('app/data-providers/selections');
 
 module.exports = function(req, res, next) {
-    selectionsProvider.getSelections().then(selections => {
+    selectionsProvider.getSelections({
+        days: 30
+    }).then(selections => {
         res.locals.dataContainer({
             page: 'main',
             selections
